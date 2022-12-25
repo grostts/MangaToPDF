1ddd0aa6ca3193cf980a06d3380474ab8f3ee834
grostts <73026090+grostts@users.noreply.github.com>
2022-12-25 08:26:53 +0300

    Second commit. I'm starting to write a function.

--- MangaToPDF.py
+++ MangaToPDF.py
@@ -3,3 +3,21 @@ from bs4 import BeautifulSoup
 import img2pdf
 import os
 
+def get_data(manga, book, chapter):
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.'
+                      '4577.63 Safari/537.36'
+    }
+
+    url = f'https://mangapoisk.ru/manga/{manga}/chapter/{book}-{chapter}'
+    print(url)
+
+# manga = input('Enter the name of the manga: ')
+# book = input('Enter the number of the book: ')
+# chapter = input('Enter the number of the chapter: ')
+
+manga = "berserk"
+book = 42
+chapter = 368
+
+get_data(manga, book, chapter)
